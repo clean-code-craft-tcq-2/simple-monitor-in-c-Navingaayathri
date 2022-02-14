c05c9a7c06416268ced04394c6b0424738cddc4b
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-02-15 02:37:34 +0530

    Update BMSCheck.h

--- BMSCheck.h
+++ BMSCheck.h
@@ -5,6 +5,7 @@
 #define   MIN_BATT_CHARGE_RATE 0
 #define   MAX_BATT_CHARGE_RATE 0.8
 
+extern bool checkValidRange(float inputValue, float MinThresholdvalue, float MaxThresholdvalue);
 extern bool checkBatteryTemperature(float temperature, float MIN_BATT_TEMP, float MAX_BATT_TEMP);
 extern bool checkBatterySoC(float SoC, float MIN_BATT_SoC, float MAX_BATT_SoC) ;
 extern bool checkBatteryChargeRate(float chargeRate, float MIN_BATT_CHARGE_RATE, float MAX_BATT_CHARGE_RATE);
