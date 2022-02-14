230d1b263339896b5da56ad017db7a3fe8ad41b3
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-02-15 02:30:47 +0530

    Update BMSCheck.h

--- BMSCheck.h
+++ BMSCheck.h
@@ -1,12 +1,14 @@
-#define MIN_THRESHOLD_BATT_TEMP 0
-#define MAX_THRESHOLD_BATT_TEMP 45
-#define MIN_THRESHOLD_BATT_SoC 20
-#define MAX_THRESHOLD_BATT_SoC 80
-#define MAX_THRESHOLD_BATT_CHARGE_RATE 0.8
+#define   MIN_BATT_TEMP 0
+#define   MAX_BATT_TEMP 45
+#define   MIN_BATT_SoC 20
+#define   MAX_BATT_SoC 80
+#define   MIN_BATT_CHARGE_RATE 0
+#define   MAX_BATT_CHARGE_RATE 0.8
 
-extern int checkBatteryTemperature(float temperature);
-extern int checkBatterySoC(float SoC);
-extern int checkBatteryChargeRate(float chargeRate);
-extern int checkBatterySoH(float temperature, float SoC, float chargeRate);
-int printOnConsole(const char* statement);
-extern int (*fpPrintOnConsole) (const char*);
+extern bool checkBatteryTemperature(float temperature, float MIN_BATT_TEMP, float MAX_BATT_TEMP);
+extern bool checkBatterySoC(float SoC, float MIN_BATT_SoC, float MAX_BATT_SoC) ;
+extern bool checkBatteryChargeRate(float chargeRate, float MIN_BATT_CHARGE_RATE, float MAX_BATT_CHARGE_RATE);
+extern int BatteryStateOk(float temperature, float SoC , float chargeRate, bool StateOk);
+extern int BatteryStateNOk(float temperature, float SoC , float chargeRate, bool StateNOk);
+extern void displayonConsole();
+extern void main();
