726a29f1aa41ee4b618d68f93295d47b0c5d650b
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-02-16 13:43:22 +0530

    Update BMSCheck.h

--- BMSCheck.h
+++ BMSCheck.h
@@ -1,15 +1,23 @@
-#define   MIN_BATTTEMP 0
-#define   MAX_BATTTEMP 45
-#define   MIN_BATTSoC 20
-#define   MAX_BATTSoC 80
-#define   MIN_BATTCHARGERATE 0
-#define   MAX_BATTCHARGERATE 0.8
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <assert.h>
 
-extern bool checkBatteryTemperature(float temperature);
-extern bool checkBatterySoC(float SoC) ;
-extern bool checkBatteryChargeRate(float chargeRate);
-extern int BatteryStateOk(float temperature, float SoC , float chargeRate);
-extern int BatteryStateNOk(float temperature, float SoC , float chargeRate);
-extern int testBatteryStateOk(float temperature, float SoC, float chargeRate);
-extern int testBatteryStateNOk(float temperature, float SoC, float chargeRate); 
-extern int main();
+#define  MIN_BATTTEMP 0
+#define  MAX_BATTTEMP 45
+#define  MIN_BATTSoC 20
+#define  MAX_BATTSoC 80
+#define  MIN_BATTCHARGERATE 0
+#define  MAX_BATTCHARGERATE 0.8
+
+#define NORMAL 0
+#define LOW_LEVEL_WARNING 1
+#define LOW_LEVEL_BREACH 2
+#define HIGH_LEVEL_WARNING 3
+#define HIGH_LEVEL_BREACH 4
+
+#define TOLERENCE_PERCENT 0.05
+
+#define ENGLISH 0
+#define DEUTSCH 1
